21d473094d9693e7de8cc8e981e50e394160c96f
Stephanie Ann Solis <stephaniesolis@lambdastudents.com>
2021-05-28 22:41:26 -0600

    character names display as well as their information

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,2 +1,23 @@
 // Write your Character component here
-// codegrade testing//
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterDiv = styled.div`
+width: 350px
+margin: 0 auto;
+height: 600px;
+`
+
+export default function Character(props) {
+    const {character} = props;
+    return (
+        <CharacterDiv>
+            <h2>Name: {character.name}</h2>
+            <p>Gender: {character.gender}</p>
+            <p>Height: {character.height}</p>
+            <p>Mass: {character.mass}</p>
+            <p>Birth Year: {character.birth_year}</p>
+            <p>Featured Films: {character.films.map(films => <li >{films}</li>)}</p>
+        </CharacterDiv>
+    )
+}
